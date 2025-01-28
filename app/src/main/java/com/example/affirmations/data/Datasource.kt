146f31b8387186c3ad32c4e64c644fcebab56f93
Pablo Jose Guardia <pjg22b@fsu.edu>
2025-01-28 17:52:24 -0500

    Complete step 3 of "Add a scrollable list"

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -16,23 +16,23 @@
 package com.example.affirmations.data
 
 import com.example.affirmations.R
-import com.example.affirmations.model.Recipe
+import com.example.affirmations.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    fun loadAffirmations(): List<Recipe> {
-        return listOf<Recipe>(
-            Recipe(R.string.affirmation1, R.drawable.image1),
-            Recipe(R.string.affirmation2, R.drawable.image2),
-            Recipe(R.string.affirmation3, R.drawable.image3),
-            Recipe(R.string.affirmation4, R.drawable.image4),
-            Recipe(R.string.affirmation5, R.drawable.image5),
-            Recipe(R.string.affirmation6, R.drawable.image6),
-            Recipe(R.string.affirmation7, R.drawable.image7),
-            Recipe(R.string.affirmation8, R.drawable.image8),
-            Recipe(R.string.affirmation9, R.drawable.image9),
-            Recipe(R.string.affirmation10, R.drawable.image10))
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation5, R.drawable.image5),
+            Affirmation(R.string.affirmation6, R.drawable.image6),
+            Affirmation(R.string.affirmation7, R.drawable.image7),
+            Affirmation(R.string.affirmation8, R.drawable.image8),
+            Affirmation(R.string.affirmation9, R.drawable.image9),
+            Affirmation(R.string.affirmation10, R.drawable.image10))
     }
 }
--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -3,7 +3,7 @@ package com.example.affirmations.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Recipe(
+data class Affirmation(
     @StringRes val stringResourceId: Int,
     @DrawableRes val imageResourceId: Int,
     //@StringRes val descriptionResourceId: Int,
