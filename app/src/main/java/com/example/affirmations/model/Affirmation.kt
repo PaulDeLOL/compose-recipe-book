9ceb17c985d0ada3c636784a85800d3f341fad66
Pablo Jose Guardia <pjg22b@fsu.edu>
2025-01-28 19:58:59 -0500

    Change presentation of app from "Affirmations" to a "Recipe Book"

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -6,7 +6,4 @@ import androidx.annotation.StringRes
 data class Affirmation(
     @StringRes val stringResourceId: Int,
     @DrawableRes val imageResourceId: Int,
-    //@StringRes val descriptionResourceId: Int,
-    //@StringRes val ingredientsResourceId: Int,
-    //@StringRes val directionsResourceId: Int
 )
